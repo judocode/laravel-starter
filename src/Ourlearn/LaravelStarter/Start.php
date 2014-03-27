2317f4c15227780a2f6a0893b3c9e588146ed879
jrenton <jrenton87@gmail.com>
2014-03-26 23:43:21 -0500

    undo relationship change from file

--- src/Ourlearn/LaravelStarter/Start.php
+++ src/Ourlearn/LaravelStarter/Start.php
@@ -696,7 +696,7 @@ class Start
                 }
             }
 
-            if($continue && !$this->fromFile) {
+            if($continue) {
                 $content = \File::get($relatedModelFile);
                 if (preg_match("/function ".$this->model->lower()."/", $content) !== 1 && preg_match("/function ".$this->model->plural()."/", $content) !== 1) {
                     $index = 0;
